2fd9d6a97c9daab749373653b48bd49bd880b7d0
Ubuntu <azureuser@tmvm.fe5dkvmj55cerffxq4kgf5ew1g.bx.internal.cloudapp.net>
2025-05-28 23:57:14 +0000

    fixing prod listener defaults

--- cron/gwtm_listener_run.py
+++ cron/gwtm_listener_run.py
@@ -1,7 +1,7 @@
 from gwtm_cron import gwtm_listener
 
 if __name__ == "__main__":
-    l = gwtm_listener.listener.Listener(listener_type="LIGO_ALERT")
+    l = gwtm_listener.listener.Listener(listener_type="LIGO_ALERT", config_path="/home/azureuser/cron/listener_config.json")
     l.run(write_to_s3=True, verbose=True, dry_run=False)
 
 
